599dbbac0eef90f220f2098f9358214b25faf973
Alexander Engelsberger <aengelsb@hs-mittweida.de>
2018-06-23 16:43:12 +0200

    Stau beschleunigen

--- Assets/goalScript.cs
+++ Assets/goalScript.cs
@@ -3,19 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class goalScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     private void OnTriggerEnter(Collider other)
     {
-        other.GetComponent<playerScript>().stoptime();
+		if (other.GetComponent<playerScript> () != null) {
+			other.GetComponent<playerScript>().stoptime();
+		}
     }
 }
--- Assets/trafficJamController.cs
+++ Assets/trafficJamController.cs
@@ -9,11 +9,12 @@ public class trafficJamController : MonoBehaviour {
 	private float jamSpeed = 0.5f;
 
 	private float angerlevel = 0.1f;
+	private float nextangerlevel = 0.1f;
 
-	public float maxSpeed = .5f;
-	public float endSpeed = 0.3f;
-	public float sweetSpot = 0.7f;
-	public float honkFactor = .2f;
+	private float maxSpeed = .5f;
+	private float endSpeed = 0.3f;
+	private float sweetSpot = 0.7f;
+	private float honkFactor = .2f;
 
 	private float offsetfunction(float startx, float starty, float endx, float endy, float x){
 		float t = endy - starty;
@@ -22,18 +23,18 @@ public class trafficJamController : MonoBehaviour {
 		return (-2 * t) / (l * l * l) * (x * x * x) + (3 * t) / (l * l) * (x * x);
 	}
 
-	private float angerLevel2Speed(float angerLevel){
-		if (angerLevel < 0) {
+	private float angerLevel2Speed(float level){
+		if (level < 0) {
 			return 0;
 		}
-		else if (angerlevel >= 1){
+		else if (level >= 1){
 			return endSpeed;
 		}
-		else if (angerlevel >= 0 && angerlevel < sweetSpot){
-			return offsetfunction(0, 0, sweetSpot, maxSpeed, angerlevel);
+		else if (level >= 0 && level < sweetSpot){
+			return offsetfunction(0, 0, sweetSpot, maxSpeed, level);
 		}
-		else if (sweetSpot >= 0 && angerlevel < 1){
-			return offsetfunction(sweetSpot, maxSpeed, 1, endSpeed, angerlevel);
+		else if (sweetSpot >= 0 && level < 1){
+			return offsetfunction(sweetSpot, maxSpeed, 1, endSpeed, level);
 		}
 		else{
 			return 0;
@@ -41,13 +42,12 @@ public class trafficJamController : MonoBehaviour {
 	}
 
 	private float angerlevelToHonkEfficiency(float level){
-		return level;
+		return level + 0.1f;
 	}
 
 	public void honk(){
-		angerlevel += angerlevelToHonkEfficiency (angerlevel)*honkFactor;
-		angerlevel = Mathf.Min (1, Mathf.Max (0, angerlevel));
-		print (angerlevel);
+		nextangerlevel = angerlevel + angerlevelToHonkEfficiency (angerlevel)*honkFactor;
+		nextangerlevel = Mathf.Min (1, Mathf.Max (0, nextangerlevel));
 	}
 
 	public void playerEnter(){
@@ -63,18 +63,14 @@ public class trafficJamController : MonoBehaviour {
 	public float getJamSpeed(){
 		return angerLevel2Speed(angerlevel);
 	}
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (currentMode == Mode.playerinside) {
-			// angerlevel -= 0.01f;
+			angerlevel = nextangerlevel - 0.001f;
 			angerlevel = Mathf.Min (1, Mathf.Max (0, angerlevel));
 			transform.Translate (Vector3.forward * getJamSpeed());
+			nextangerlevel = angerlevel;
 		}
 	}
 }
